8f43714206647f7255fe3eee65aeebc217ae3f82
maryaammm <67693648+maryaammm@users.noreply.github.com>
2020-07-09 16:24:57 +0200

    Add files via upload

---
+++ Unity/Script/RotateB.cs
@@ -0,0 +1,38 @@
+﻿// source: https://www.youtube.com/watch?v=ElmzIq6stNI 
+// by applying changes described here https://forum.unity.com/threads/ivirtualbuttoneventhandler-could-not-be-found.914441/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class RotateB : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject vbBtnObj;
+    public Animator starAni;
+
+    void Start()
+    {
+    	vbBtnObj = GameObject.Find("RotationB");
+    	vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+ 
+    	starAni.GetComponent<Animator>();
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+    	starAni.Play("Star-Animation");
+    	Debug.Log("BTN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+    	starAni.Play("none");
+    	Debug.Log("BTN Released");
+    }
+
+}
