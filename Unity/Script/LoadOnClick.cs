ae1780b9ab315e9bbcc1333da8e5ad27dc052b43
maryaammm <67693648+maryaammm@users.noreply.github.com>
2020-08-15 12:06:48 +0200

    Add files via upload

---
+++ Unity/Script/LoadOnClick.cs
@@ -0,0 +1,14 @@
+﻿// source: https://learn.unity.com/tutorial/live-sessions-on-ui#5c7f8528edbc2a002053b4b3
+
+using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+   
+    public void LoadScene(int level)
+    {
+       
+        Application.LoadLevel(level);
+    }
+}
---
+++ Unity/Script/TextB.cs
@@ -0,0 +1,16 @@
+﻿// source: https://www.youtube.com/watch?v=xpcyZdyO5P8
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextB : MonoBehaviour
+{
+    public Text Textfield;
+
+    public void SetText(string text)
+    { 
+    	Textfield.text = text;
+    } 
+}
